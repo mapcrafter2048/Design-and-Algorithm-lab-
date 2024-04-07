0c0971577e65f38cdf15738463c6ad8770944ce9
mapcrafter2048 <121042282+mapcrafter2048@users.noreply.github.com>
2024-04-07 22:36:46 +0530

    Added Question 1

--- Assignment 8/Question 1/Question-1.cpp
+++ Assignment 8/Question 1/Question-1.cpp
@@ -17,6 +17,95 @@
 using namespace std;
 using namespace std::chrono;
 
+/// @brief This function is used to compare the weight of two edges
+/// @param A
+/// @param B
+bool compare(vector<int> &A, vector<int> &B)
+{
+    return A[2] > B[2];
+}
+
+/// @brief This function is used to create a set of n elements with parent and rank as 0 for each element.
+/// @param parent The parent of each element
+/// @param rank The rank of each element
+/// @param n The number of elements
+void makeSet(vector<int> &parent, vector<int> &rank, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        parent[i] = i;
+        rank[i] = 0;
+    }
+}
+
+/// @brief This function is used to find the parent of a node
+/// @param parent The parent of each element
+/// @param node The node whose parent is to be found
+/// @return The parent of the node
+int findParent(vector<int> &parent, int node)
+{
+    if (parent[node] == node)
+    {
+        return node;
+    }
+
+    return parent[node] = findParent(parent, parent[node]);
+}
+
+/// @brief This function is used to perform the union operation on two sets u and v with parent and rank as parent and rank respectively.
+/// @param u The first set
+/// @param v The second set
+/// @param parent The parent of each element
+/// @param rank The rank of each element
+void unionFind(int u, int v, vector<int> &parent, vector<int> &rank)
+{
+    u = findParent(parent, u);
+    v = findParent(parent, v);
+
+    if (rank[u] < rank[v])
+    {
+        parent[u] = v;
+    }
+    else if (rank[u] > rank[v])
+    {
+        parent[v] = u;
+    }
+    else
+    {
+        parent[u] = v;
+        rank[v]++;
+    }
+}
+
+/// @brief This function is used to find the minimum spanning tree cost of a graph with n nodes and edges as edges.
+/// @param edges The edges of the graph
+/// @param n The number of nodes in the graph
+/// @return The minimum spanning tree cost of the graph
+int minimumSpanningTreeCost(vector<vector<int>> &edges, int n)
+{
+    sort(edges.begin(), edges.end(), compare);
+
+    vector<int> parent(n), rank(n);
+    makeSet(parent, rank, n);
+
+    int maxWeight = 0;
+
+    for (int i = 0; i < edges.size(); i++)
+    {
+        int u = findParent(parent, edges[i][0]);
+        int v = findParent(parent, edges[i][1]);
+        int wt = edges[i][2];
+
+        if (u != v)
+        {
+            maxWeight += wt;
+            unionFind(u, v, parent, rank);
+        }
+    }
+
+    return maxWeight;
+}
+
 int main()
 {
     auto start = high_resolution_clock::now();
